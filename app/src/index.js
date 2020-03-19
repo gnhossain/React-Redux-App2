578b21ae0c450be4f9c9b02cc17c886a3affee27
gul hossain <spicymangojungle@gmail.com>
2020-03-18 21:58:54 -0700

    index import libray and add loader

--- app/src/index.js
+++ app/src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../src/reducer/index';
+
+
+const composeEnhancers = window.__REDUX_DVETOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+    rootElement)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
